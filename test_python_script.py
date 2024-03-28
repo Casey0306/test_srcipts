1bf99d9aef2e16be1d9b2180f80433fb0603653a
Kirill <53292165+Casey0306@users.noreply.github.com>
2024-03-28 19:43:45 +0300

    Update test_python_script.py

--- test_python_script.py
+++ test_python_script.py
@@ -4,5 +4,11 @@ if __name__ == "__main__":
     for param in sys.argv:
        print(param)
        print(type(param))
+    print("Username:")
+    print(sys.argv[0])
+    print("Password:")
+    print(sys.argv[1])
+    print("Ip address:")
+    print(sys.argv[2])
 
 print("test_python_script")
