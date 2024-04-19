10bc5b6e00f8eddb8ed9bfd9dd31903f1a1868c9
Kirill <53292165+Casey0306@users.noreply.github.com>
2024-04-19 12:45:55 +0300

    Update show_run.py

--- show_run.py
+++ show_run.py
@@ -1,11 +1,15 @@
-import paramiko 
-host1 = '192.168.200.10'
-user = 'admin'
-secret = 'P@ssword'
+import sys
+import paramiko
+
+host1 = sys.argv[4]
+user = sys.argv[1]
+password = sys.argv[2]
+enable_password = sys.argv[3]
+
 port = 22
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
-ssh.connect(hostname=host1, username=user, password=secret, port=port)
+ssh.connect(hostname=host1, username=user, password=password, port=port)
 stdin, stdout, stderr = ssh.exec_command('show clock')
 list = stdout.readlines()
 print(list)
