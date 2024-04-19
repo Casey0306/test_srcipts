66081d35c020c804f6b365d50a88c53c0b930f49
Kirill <53292165+Casey0306@users.noreply.github.com>
2024-04-19 14:26:35 +0300

    Create show_version.py

---
+++ show_version.py
@@ -0,0 +1,25 @@
+import paramiko
+import sys
+
+
+ip_address = sys.argv[4]
+username = sys.argv[1]
+password = sys.argv[2]
+enable_password =  sys.argv[3]
+
+print(ip_address)
+print(type(ip_address))
+print(username)
+print(password)
+print(enable_password)
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(ip_address, username=username, password=password)
+
+command = "show version"
+stdin, stdout, stderr = client.exec_command(command)
+output = stdout.read().decode()
+print(output)
+
+client.close()
