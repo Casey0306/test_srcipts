3ff8fb655499bca34671ae9c7164966eff341d7f
Kirill <53292165+Casey0306@users.noreply.github.com>
2024-04-25 20:23:44 +0300

    Update netmiko_test.py

--- netmiko/netmiko_test.py
+++ netmiko/netmiko_test.py
@@ -15,11 +15,17 @@ cisco = {
     "password": password,
     "secret": enable_password,
 }
+if ip_address == "10.219.2.57":
+    neighbor = "neighbor 1.1.1.57 remote-as 10"
+if ip_address == "10.219.2.158":
+    neighbor = "neighbor 1.1.1.158 remote-as 10"
 
+commands = ["router bgp 2000", neighbor]
 with ConnectHandler(**cisco) as net_connect:
   net_connect.enable()
-  output1 = net_connect.send_command("show run")
-  output2 = net_connect.send_command("show interfaces")
+  output = net_connect.send_command("show run")
+  output += net_connect.send_command("show interfaces")
+  output += net_connect.send_config_set(commands)
+  output += net_connect.save_config()
 
-print(output1)
-print(output2)
+print(output)
